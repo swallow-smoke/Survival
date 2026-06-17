3ac344541876647bdf04f8fd3afc53b85a9f24a1
swallow-smoke <teatimesucream@gmail.com>
2026-06-17 20:16:46 +0900

    Fix | Change input action assets

---
+++ Assets/001_Scripts/000_Core/SystemScope.cs
@@ -0,0 +1,16 @@
+﻿using MessagePipe;
+using VContainer;
+using VContainer.Unity;
+
+namespace _001_Scripts.Core
+{
+    public class SystemScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            var options = builder.RegisterMessagePipe();
+            
+            
+        }
+    }
+}
